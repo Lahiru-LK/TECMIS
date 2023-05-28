d70ee958349d8dd100b09983dc180d570c055f1d
DHANANJAYA RUPASINGHE <106276257+Dhananjaya0216@users.noreply.github.com>
2023-05-28 19:56:04 +0530

    8

---
+++ TECMIS/src/main/java/com/tecmis/project/Student/Database.java
@@ -0,0 +1,13 @@
+//package com.tecmis.project.Student;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//
+//public class Database {
+//    public static Connection connectDb(){
+//        try{
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection connect  = DriverManager.get
+//        }
+//    }
+//}
